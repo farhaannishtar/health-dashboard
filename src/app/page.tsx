343f7fd58e22df8600c5e5e2122f82fca04ae6eb
Farhaan Nishtar <farhaanmnishtar99@gmail.com>
2023-08-25 12:38:33 -0400

    added imports into page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 import Image from 'next/image'
+import { useRouter } from 'next/router'
+import crypto from 'crypto';
+import Time from './components/Time';
+import { getCurrentDate, getPreviousDate, calculatePercentageChange } from '../helpers/helpers';
 
 export default function Home() {
   return (
     <div>
       <h1>Home</h1>
+      <Time />
     </div>
   )
 }
